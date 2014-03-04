cdac56e038e0f85038b1531ce786afd7a98d45bb
Sam Harris <sharris@trinity.edu>
2014-03-04 14:55:44 -0600

    Save files locally JSON, Contextual action bar, Delete functionality

--- app/src/main/java/com/samiamharris/criminalintent/CrimeLab.java
+++ app/src/main/java/com/samiamharris/criminalintent/CrimeLab.java
@@ -1,6 +1,8 @@
 package com.samiamharris.criminalintent;
 
 import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.UUID;
@@ -11,14 +13,27 @@ import java.util.UUID;
 //Singleton pattern
 public class CrimeLab {
 
-    private static CrimeLab sCrimeLab;
-    private Context mAppContext;
+    private static final String TAG = "CrimeLab";
+    private static final String FILENAME = "crimes.json";
+
     private ArrayList<Crime> mCrimes;
+    private CriminalIntentJSONSerializer mSerializer;
+
+    private Context mAppContext;
+    private static CrimeLab sCrimeLab;
 
     //Context give singleton lots of access
     private CrimeLab(Context appContext) {
         mAppContext = appContext;
-        mCrimes = new ArrayList<Crime>();
+        mSerializer = new CriminalIntentJSONSerializer(mAppContext, FILENAME);
+
+        try {
+            mCrimes = mSerializer.loadCrimes();
+        } catch (Exception e) {
+            mCrimes = new ArrayList<Crime>();
+            Log.e(TAG, "Error loading crimes: ", e);
+            Toast.makeText(mAppContext, "Error loading crimes", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public static CrimeLab get(Context c) {
@@ -26,7 +41,7 @@ public class CrimeLab {
         if(sCrimeLab == null) {
             sCrimeLab = new CrimeLab(c.getApplicationContext());
         }
-        //if it alreadye exists then return it
+        //if it already exists then return it
         return sCrimeLab;
     }
 
@@ -44,6 +59,22 @@ public class CrimeLab {
 
     public void addCrime(Crime c) {
         mCrimes.add(c);
+    }
+
+    public void deleteCrime(Crime c) {
+        mCrimes.remove(c);
+    }
+
+    public boolean saveCrimes() {
+        try {
+            mSerializer.saveCrimes(mCrimes);
+            Log.d(TAG, "crimes saved to file");
+            return true;
+        } catch (Exception e) {
+            Toast.makeText(mAppContext, "Error saving crimes", Toast.LENGTH_SHORT).show();
+            Log.e(TAG, "Error saving crimes: ", e);
+            return false;
+        }
     }
 
 
